b1163886ea7a28bb053f7745e7b0fd35cc4cb221
Ryosuke <tffcd0514@gmail.com>
2021-12-05 15:04:25 +0900

    add: ngrok使用のため開発環境の設定を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # ngrokを使用するため、全ホストからのアクセスを許可
+  config.hosts.clear
 end
