e3765de1c164eb2254fd2a51fdb112961ebb00c8
Ryosuke <tffcd0514@gmail.com>
2021-12-13 05:11:35 +0900

    add: statusカラムにenumを適用 #21

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   validates :character_rate_setting, numericality: { in: 1..5  }, allow_nil: true
   validates :fotune_telling_rate_setting, numericality: { in: 1..5  }, allow_nil: true
 
+  enum status: { normal: 0, name_registration: 1, reading_registration: 2 }
+
   before_create -> { self.uuid = SecureRandom.uuid }
 end
