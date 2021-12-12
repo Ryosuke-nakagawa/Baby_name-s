b9d216bee9fe577564d0e18ce3007d31fd7ba07d
Ryosuke <tffcd0514@gmail.com>
2021-12-12 14:33:22 +0900

    add: LINEのメッセージを受け取れることを確認 #21

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,6 +3,25 @@ class LineBotController < ApplicationController
 
   def callback
     body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE'] #リクエストのヘッダーの署名情報を取得
+    unless client.validate_signature(body, signature) #署名検証を行う https://developers.line.biz/ja/docs/messaging-api/receiving-messages/#verifying-signatures
+      return head :bad_request
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+            }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
   end
 
   private
