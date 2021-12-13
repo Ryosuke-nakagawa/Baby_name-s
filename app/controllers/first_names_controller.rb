9a1520dbade73bae5a85472dae7ec7bdce525715
Ryosuke <tffcd0514@gmail.com>
2021-12-14 04:53:26 +0900

    add: first_nameコントローラー作成 #22

---
+++ app/controllers/first_names_controller.rb
@@ -0,0 +1,14 @@
+class FirstNamesController < ApplicationController
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
---
+++ app/helpers/first_names_helper.rb
@@ -0,0 +1,2 @@
+module FirstNamesHelper
+end
