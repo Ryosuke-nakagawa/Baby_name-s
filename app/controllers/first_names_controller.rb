750c3fb7fece6463b19bc64ac2cc25fbafb41495
Ryosuke <tffcd0514@gmail.com>
2021-12-14 09:57:18 +0900

    add: controllerにログイン用の記述を追加

--- app/controllers/first_names_controller.rb
+++ app/controllers/first_names_controller.rb
@@ -1,6 +1,26 @@
 class FirstNamesController < ApplicationController
+  before_action :set_first_names_liffid, only: [:login]
+
+  require 'net/http'
+  require 'uri'
+
+  def login
+    if params[:idToken]
+      idToken = params[:idToken]
+      channelId = ENV['LIFF_CHANNEL_ID']
+      res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'),{'id_token'=>idToken, 'client_id'=>channelId})
+      line_user_id = JSON.parse(res.body)["sub"]
+      user = User.find_by(line_id: line_user_id)
+
+      session[:user_id] = user.id
+      group_id = { id: user.group_id }
+      render json: group_id
+    end
+  end
 
   def index
+    @user = User.find(session[:user_id])
+    @first_names = @user.group.first_names
   end
 
   def update
@@ -10,5 +30,7 @@ class FirstNamesController < ApplicationController
   end
 
   def show
+    @first_name = FirstName.find(params[:id])
+    @group = Group.find(params[:group_id])
   end
 end
