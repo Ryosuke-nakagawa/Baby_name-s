9bce92b8502dd010cdc1ec1875b5400da8696e9d
Ryosuke <tffcd0514@gmail.com>
2021-12-25 09:12:39 +0900

    add: ソートすると、遷移先で現在のソートが表示される様に変更 #31

--- app/controllers/first_names_controller.rb
+++ app/controllers/first_names_controller.rb
@@ -22,20 +22,24 @@ class FirstNamesController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
-    @sort = params[:sort]
+
     case params[:sort]
     when 'sound'
       @first_names = []
       result = FirstName.where(group_id: @group.id).joins(:rates).group(:id).order('average_rates_sound_rate DESC').average("rates.sound_rate")
       result.map{ |first_name_id, average| @first_names << FirstName.find(first_name_id) }
+      @sort = "音の響き"
     when 'character'
       @first_names = []
       result = FirstName.where(group_id: @group.id).joins(:rates).group(:id).order('average_rates_character_rate DESC').average("rates.character_rate")
       result.map{ |first_name_id, average| @first_names << FirstName.find(first_name_id) }
+      @sort = "漢字"
     when 'fotune_telling'
       @first_names = FirstName.where(group_id: @group.id).order(fotune_telling_rate: :DESC)
+      @sort = "姓名判断"
     else
       @first_names = FirstName.sort_by_overall_rating(@group.first_names,@group.users)
+      @sort = "総評価"
     end
   end
 
