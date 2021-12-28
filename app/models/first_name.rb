3843615f5699b8ecd1ccc71b1cd1345269d231cf
Ryosuke <tffcd0514@gmail.com>
2021-12-29 06:00:44 +0900

    add: indexページのバグ修正 #35

--- app/models/first_name.rb
+++ app/models/first_name.rb
@@ -9,17 +9,29 @@ class FirstName < ApplicationRecord
   def self.sort_by_overall_rating(first_names,group_users)
     score = {}
 
-    first_names.each do |first_name|
+    first_names.each_with_index do |first_name, i|
       sum = 0
+      user_count = group_users.count
       group_users.each do |user|
         rate = Rate.find_by(first_name: first_name,user: user)
-        break if rate.nil?
+        if rate.nil?
+          user_count -= 1
+          next
+        end
+        # userの評価基準と、実際の評価を掛け合わせて総評価の点をsumで出力
         sound_rate = rate.sound_rate * user.sound_rate_setting
         character_rate = rate.character_rate * user.character_rate_setting
         fotune_telling_rate = first_name.fotune_telling_rate * user.fotune_telling_rate_setting
         sum = sound_rate + character_rate + fotune_telling_rate
       end
-      result = sum / group_users.count
+      if user_count.zero?
+        # 名前の評価がまだない場合1以下で定義
+        result = "0.#{i}1".to_f
+      else
+        result = sum / user_count
+        # 同点で被るとハッシュ変換時に上書きしてしまうので、一意の数にするためにiを少数に追加
+        result = "#{result}.#{i}1".to_f
+      end
       score[result] = first_name # {合計点 => first_name }ができる
     end
     sorted_score = score.sort.reverse
