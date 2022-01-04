b2e65479effab18656b8b11810cfdb9faf04c958
Ryosuke <tffcd0514@gmail.com>
2022-01-04 15:56:38 +0900

    add: デバッグ用gemを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'),{'id_token'=>idToken, 'client_id'=>channelId})
     line_user_id = JSON.parse(res.body)["sub"]
     user = User.find_by(line_id: line_user_id)
+    binding.pry
     if params[:uuid] #人からの紹介urlの場合
       link_user = User.find_by(uuid: params[:uuid])
       user = User.create!(line_id: line_user_id, group: link_user.group)
