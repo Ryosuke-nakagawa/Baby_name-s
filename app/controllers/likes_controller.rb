923f9e49054f2e79563ae0a4cbb63caf32ec8844
Ryosuke <tffcd0514@gmail.com>
2022-01-12 06:19:43 +0900

    fix: 翻訳を修正 #57

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,12 @@ class LikesController < ApplicationController
   def create
     first_name = FirstName.find(params[:first_name_id])
     current_user.like(first_name)
-    redirect_to group_first_names_path(current_user.group), success: t('defaults.message.success')
+    redirect_to group_first_names_path(current_user.group), success: t('.success')
   end
 
   def destroy
     first_name = current_user.likes.find(params[:id]).first_name
     current_user.unlike(first_name)
-    redirect_to group_first_names_path(current_user.group), success: t('defaults.message.success')
+    redirect_to group_first_names_path(current_user.group), success: t('.success')
   end
 end
