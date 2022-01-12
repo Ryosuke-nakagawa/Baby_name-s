4790fb243e500836d552c87220d9b6444016eab7
Ryosuke <tffcd0514@gmail.com>
2022-01-12 13:24:20 +0900

    add: controllerをajax用に編集 #59

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,12 @@
 class LikesController < ApplicationController
 
   def create
-    first_name = FirstName.find(params[:first_name_id])
-    current_user.like(first_name)
-    redirect_to group_first_names_path(current_user.group), success: t('.success')
+    @first_name = FirstName.find(params[:first_name_id])
+    current_user.like(@first_name)
   end
 
   def destroy
-    first_name = current_user.likes.find(params[:id]).first_name
-    current_user.unlike(first_name)
-    redirect_to group_first_names_path(current_user.group), success: t('.success')
+    @first_name = current_user.likes.find(params[:id]).first_name
+    current_user.unlike(@first_name)
   end
 end
