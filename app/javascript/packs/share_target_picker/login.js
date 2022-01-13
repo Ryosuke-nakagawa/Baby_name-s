304b3cad2bb95e980346e6958f0b594210bc9779
Ryosuke <tffcd0514@gmail.com>
2022-01-13 10:57:04 +0900

    add: 招待メッセージにflexmessageが使えることを確認 #62

--- app/javascript/packs/share_target_picker/login.js
+++ app/javascript/packs/share_target_picker/login.js
@@ -23,17 +23,67 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     fetch(request)
     .then(response => {
-      console.log(response.json); // => 200
       return response.json().then(response_uuid => {
         // JSONパースされたオブジェクトが渡される
         var uuid = response_uuid ;
         if (liff.isApiAvailable('shareTargetPicker')) {
           const url = `https://liff.line.me/1656693818-N2kw6Bvk?q=${uuid.id}`
           liff.shareTargetPicker([{
-            type: "text",
-            text: url
+            "type": "flex",
+            "altText": "Baby names招待メッセージが届きました",
+            "contents": {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://enamae.net/result2/%E4%B8%AD%E5%B7%9D__%E5%87%8C%E8%BC%94.jpg",
+                "size": "full",
+                "aspectRatio": "6:5",
+                "aspectMode": "cover",
+                "action": {
+                  "type": "uri",
+                  "uri": "http://linecorp.com/"
+                },
+                "position": "relative"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "weight": "bold",
+                    "size": "sm",
+                    "text": "名前を評価・保存・シェアするアプリ",
+                    "margin": "none",
+                    "align": "center"
+                  }
+                ],
+                "spacing": "none"
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                      "type": "uri",
+                      "label": "アカウントをシェアしてはじめる",
+                      "uri": url
+                    }
+                  },
+                  {
+                    "type": "spacer",
+                    "size": "sm"
+                  }
+                ],
+                "flex": 0
+              }
             },
-          ],
+          }],
           ).catch(function (res) {
             //「シェアターゲットピッカー」が有効になっているが取得に失敗した場合 
             console.log('LINEのバージョンが古い可能性があります');
