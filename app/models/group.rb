e627c9d0217a7913c73fb66be6cb13bcda2fe5ca
Ryosuke <tffcd0514@gmail.com>
2022-01-20 11:42:49 +0900

    add: cssを追加 #106

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,8 @@ class Group < ApplicationRecord
   accepts_nested_attributes_for :users
 
   validates :last_name, length: { maximum: 20 }, allow_nil: true
+
+  def two_or_more_user?
+    users.count >= 2
+  end
 end
