e644109c7002cff2dab8f428bb72c060bbe327a1
Ryosuke <tffcd0514@gmail.com>
2022-01-27 16:57:59 +0900

    add: first_names_specの大枠作成 #114

--- spec/system/first_names_spec.rb
+++ spec/system/first_names_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "FirstNames", type: :system do
+  let!(:user) { create(:user) }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  before do
+    # session[:user_id]に値を入れユーザーがログインしている状態を作る
+    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(user_id: user.id)
+  end
 
-
+  describe '名前一覧画面' do
+    context '正常系' do
+      it '一覧ページにアクセスした場合、名前が表示されること' do
+      end
+    end
+  end
+  describe '名前新規作成' do
+    context '正常系' do
+      it '名前が新規作成されること' do
+      end
+    end
+  end
+  describe '名前詳細' do
+    context '正常系' do
+      it '名前が表示されること' do
+      end
+    end
+  end
+  describe '名前削除' do
+    context '正常系' do
+      it '名前が削除されること' do
+      end
+    end
+  end
 end
