0ce27f33d90b13cb16b9b94d38b4392b04c2f72b
Ryosuke <tffcd0514@gmail.com>
2022-01-27 16:57:11 +0900

    add: capybaraの設定 #114

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,8 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chome, screen_size: [1920, 1080]
+    # ブラウザon
+    driven_by(:selenium_chrome)
+    # ブラウザoff
+    # driven_by(:selenium_chrome_headless)
   end
 end
