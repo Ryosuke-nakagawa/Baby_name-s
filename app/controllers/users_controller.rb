c398b39e83280df13dbc8e40a8322f3769e2ca33
Ryosuke <tffcd0514@gmail.com>
2022-01-29 15:00:48 +0900

    add: line_authenticateサービスクラスを作成 #116

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,8 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    id_token = params[:idToken]
-    channel_id = ENV['LIFF_CHANNEL_ID']
-    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'),
-                              { 'id_token' => id_token, 'client_id' => channel_id })
-    line_user_id = JSON.parse(res.body)['sub']
-    user = User.find_by(line_id: line_user_id)
+    line_authenticate_service = LineAuthenticateService.new(params[:idToken])
+    user = User.find_by(line_id: lineid_authenticate_service.get_line_id)
     if params[:uuid] # 人からの紹介urlの場合
       link_user = User.find_by(uuid: params[:uuid])
       if user.nil?
