05b0d9a92ec7e2516671313c21ba0dd5e3c1da62
Ryosuke <tffcd0514@gmail.com>
2022-01-31 15:54:00 +0900

    add: rankingsコントローラーをリファクタリング #95

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -8,10 +8,8 @@ class RankingsController < ApplicationController
       @selected_year = Ranking.maximum(:year)
     end
     years = Ranking.select(:year).group(:year).order(year: :desc)
-    @saved_years= []
-    years.each do |year|
-      @saved_years << year[:year]
-    end
+    @saved_years= years.pluck('year')
+
     boy_first_names = Ranking.where(year: @selected_year,sex: "boy")
     girl_first_names = Ranking.where(year: @selected_year,sex: "girl")
     @ranking_first_names = [boy_first_names, girl_first_names].transpose
