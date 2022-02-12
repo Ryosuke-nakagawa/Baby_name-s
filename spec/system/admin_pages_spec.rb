7c93405bbeffcab80b30451f9b579429ec4ba329
Ryosuke <tffcd0514@gmail.com>
2022-02-13 07:21:22 +0900

    add: adminページのsystemspec大枠作成 #130

---
+++ spec/system/admin_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "AdminPages", type: :system do
+  let(:admin_user) { create(:user, :admin) }
+  let!(:another_user) { create(:user, :general) }
+  before do
+    # session[:user_id]に値を入れユーザーがログインしている状態を作る
+    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(user_id: admin_user.id)
+  end
+
+  describe '管理画面' do
+    context '正常系' do
+      it 'adminユーザーは管理画面のダッシュボードページに遷移できる' do
+      end
+      it '一覧ページで「Show」をクリックすると、名前が表示れること' do
+      end
+      it 'ログアウトボタンで管理画面からトップページに遷移する' do
+      end
+    end
+    context '異常系' do
+      it 'adminユーザーではないユーザーは管理画面に遷移できない' do
+      end
+    end
+  end
+end
