9204fb9f3130d00f5ec0943514d3735c28b89428
Ryosuke <tffcd0514@gmail.com>
2022-06-11 06:08:19 +0900

    add: コメント編集機能追加 #142

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,22 @@ class CommentsController < ApplicationController
     @comment.destroy!
   end
 
+  def update
+    @comment = current_user.comments.find(params[:id])
+    if @comment.update(comment_update_params)
+      render json: { comment: @comment }, status: :ok
+    else
+      render json: { comment: @comment, errors: { messages: @comment.errors.full_messages } }, status: :bad_request
+    end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:body).merge(first_name_id: params[:first_name_id])
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:body)
   end
 end
