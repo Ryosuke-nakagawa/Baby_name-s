c534c652dc45f652451baf2df7a4bd027cc68080
Ryosuke <tffcd0514@gmail.com>
2022-06-11 16:23:56 +0900

    add: コメントモデルにバリデーション追加 #142

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_055117) do
+ActiveRecord::Schema.define(version: 2022_06_11_071746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
